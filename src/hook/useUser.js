55be23000dba4dbeb9538d5e847023a981bdfaff
ihsa-mariano <mtorrecilla@emergencias.com.ar>
2021-08-24 18:22:04 -0300

    perf: update hook
    update useUser.js hook to sessionStorage for login

--- src/hook/useUser.js
+++ src/hook/useUser.js
@@ -1,19 +1,35 @@
-import { useContext, useCallback } from 'react'
+import { useContext, useCallback, useState } from 'react'
 import Context from '../context/UserContext'
+import loginService from '../services/login'
 
 export default function useUser() {
     const {jwt, setJWT} = useContext(Context)
+    const [state, setState] = useState({ loading: false, error: false })
 
-    const login = useCallback(() => {
-        setJWT('test')
+    const login = useCallback(({username, password}) => {
+        setState({ loading: true, error: false })
+        loginService({username, password})
+            .then(jwt => {
+                window.sessionStorage.setItem('jwt', jwt)
+                setState({ loading: false, error: false })
+                setJWT(jwt)
+            })
+            .catch(err => {
+                window.sessionStorage.removeItem('jwt')
+                setState({ loading: false, error: true })
+                console.error(err)
+            })
     }, [setJWT])
 
     const logout = useCallback(() => {
+        window.sessionStorage.removeItem('jwt')
         setJWT(null)
     }, [setJWT])
 
     return {
         isLogged: Boolean(jwt),
+        isLoginLoading: state.loading,
+        hasLoginError: state.error,
         login,
         logout
     }
