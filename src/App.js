d7ac2869ddfde752ebc1808c9cdd1d315ed995b7
ihsa-mariano <mtorrecilla@emergencias.com.ar>
2021-08-24 18:27:45 -0300

    perf: register page on App.js
    includes register page on App.js

--- src/App.js
+++ src/App.js
@@ -2,21 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 import Header from "./components/Header/Header"
 import Login from "./pages/Login/index"
+import Register from "./pages/Register/index"
 
 import { Route, Switch } from 'wouter';
 
 import {UserContextProvider} from "./context/UserContext"
 
 function App() {
+
   return (
     <UserContextProvider>
       <div className="App">
         <Header />
         <Switch>
-              <Route 
+            <Route 
                 component={Login}
                 path="/login"
-              />
+            />
+            <Route 
+                component={Register}
+                path="/register"
+            />
         </Switch>
         <div className="App-header">
           <h1>Login React App</h1>
