b91a4d91ee5d92c7dbe2af73a6ae06a01a60cc75
ihsa-mariano <mtorrecilla@emergencias.com.ar>
2021-08-24 09:11:31 -0300

    perf: App.js
    update App.js file to includes new components, pages,
    hooks and contexts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header/Header"
+import Login from "./pages/Login/index"
+
+import { Route, Switch } from 'wouter';
+
+import {UserContextProvider} from "./context/UserContext"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContextProvider>
+      <div className="App">
+        <Header />
+        <Switch>
+              <Route 
+                component={Login}
+                path="/login"
+              />
+        </Switch>
+        <div className="App-header">
+          <h1>Login React App</h1>
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+      </div>
+    </UserContextProvider>
   );
 }
 
