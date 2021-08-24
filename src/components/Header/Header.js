364e484dd1b48bfa1815d7e3ef7126c39458981d
ihsa-mariano <mtorrecilla@emergencias.com.ar>
2021-08-24 09:08:17 -0300

    feat: header component
    create header component for login

---
+++ src/components/Header/Header.js
@@ -0,0 +1,28 @@
+import React from "react"
+import {Link} from "wouter"
+import useUser from "../../hook/useUser"
+import './Header.css'
+
+export default function Header () {
+    //const isLogged = false
+    const {isLogged, logout} = useUser()
+
+    const handleClick = e => {
+        e.preventDefault()
+        logout()
+    }
+
+    return (
+        <header className='gf-header'>
+            { isLogged ? 
+            <Link href="#" onClick={handleClick}>
+                Logout
+            </Link>
+            :
+            <Link to='/login'>
+                Login
+            </Link>
+            }
+        </header>
+    )
+}
